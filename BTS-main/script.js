b3232b0f38e07bef7ffde8db3c670fd0b1c05180
maisa08 <159453189+maisa08@users.noreply.github.com>
2025-03-19 16:25:12 -0300

    Add files via upload

--- BTS-main/script.js
+++ BTS-main/script.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const botaoDeAcessibilidade = document.getElementById(botao-acessibilidade);
-    const opcoesDeAcessibilidade = document.getElementById(opcoes-acessibilidade);
 
-    botaoDeAcessibilidade.addEventListener('click' , function(){
+
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+
     })
+
+
+
+
 
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+
+    const alternaContraste = document.getElementById('alterna-contraste')
 
 
     let tamanhoAtualFonte = 1;
@@ -31,4 +47,22 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 
 
+   alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+     })
+    
+
+
 })
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contado', { delay: 500 });
+
+
+
+
+
+
+
